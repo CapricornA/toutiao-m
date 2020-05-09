b2f11c437553d14e2dc6f2cb212e797a320ee59b
C_apricorn_A <18746416966jk@sina.com>
2020-05-09 16:01:05 +0800

    登录模块

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      title="登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        label="手机号"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        label="验证码"
+        left-icon="music-o"
+        placeholder="请输入验证码">
+        <template #button>
+          <van-button size="small" type="primary">发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <van-button type="info" block>默认按钮</van-button>
+    <!-- /登录表单 -->
+
+  </div>
 </template>
 
 <script>
@@ -8,7 +37,12 @@ export default {
   props: {},
   components: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
