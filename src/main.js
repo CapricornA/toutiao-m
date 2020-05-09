3164bedc8b14a6c980d5156371df321359bf4e26
C_apricorn_A <18746416966jk@sina.com>
2020-05-09 10:48:14 +0800

    导入 Vant 组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载全局样式
 import './styles/index.less'
+
+// 加载 Vant 组件库
+import Vant from 'vant'
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
