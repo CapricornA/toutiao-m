b1a8bcb8a9b98de377bdc14ce850d51e83603791
C_apricorn_A <18746416966jk@sina.com>
2020-05-12 11:26:52 +0800

    我的-展示当前登录用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -2,6 +2,8 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
+// 在非组件模块中获取 Store 必须通过这种方式
+import store from '@/store/'
 
 /**
  * 登录.注册
@@ -21,5 +23,18 @@ export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+/**
+ * 获取登录用户自己信息
+ */
+export const getCurrentUser = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user',
+    headers: {
+      Authorization: `Bearer ${store.state.user.token}`
+    }
   })
 }
