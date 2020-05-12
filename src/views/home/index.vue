71c1bf3880a57d5a6438946c6ee3e46714d31367
C_apricorn_A <18746416966jk@sina.com>
2020-05-12 14:54:21 +0800

    首页文章列表-页面布局-导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        class="search-btn"
+        icon="search"
+        type="info"
+        round
+        size="samll"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script>
@@ -19,4 +32,21 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background-color: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
 </style>
