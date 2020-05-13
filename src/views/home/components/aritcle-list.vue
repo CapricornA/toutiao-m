3b2835d30467eb24a17dff08fc482def84ad4a5a
C_apricorn_A <18746416966jk@sina.com>
2020-05-13 11:03:41 +0800

    首页文章列表-展示文章列表

--- src/views/home/components/aritcle-list.vue
+++ src/views/home/components/aritcle-list.vue
@@ -6,7 +6,10 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in articles"
+        :key="index"
+        :title="article.title" />
     </van-list>
   </div>
 </template>
@@ -25,9 +28,10 @@ export default {
   components: {},
   data () {
     return {
-      list: [], // 数据列表
+      articles: [], // 数据列表
       loading: false, // 控制加载中的 loading 状态
-      finished: false // 控制加载结束的状态
+      finished: false, // 控制加载结束的状态
+      timestamp: null
     }
   },
   computed: {},
@@ -39,13 +43,24 @@ export default {
       // 1. 请求获取数据
       const { data } = await getArticles({
         channel_id: this.channel.id,
-        timestamp: Date.now(),
+        timestamp: this.timestamp || Date.now(),
         with_top: 1
       })
-      console.log(data)
       // 2. 把数据放到 list 数组中
+      const { results } = data.data
+      this.articles.push(...results)
+
       // 3. 设置本次加载状态结束, 它才可以判断是否需要加载下一次
+      this.loading = false
+
       // 4. 数据全部加载完成
+      if (results.length) {
+        // 更新获取下一页数据的页码
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        // 没有数据了, 把加载状态设置结束
+        this.finished = true
+      }
     }
   }
 }
